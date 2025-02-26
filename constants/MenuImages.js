a7eae4389f426116e76ca2b777e3d7b652d4466f
Akhilsaireddy2005 <akhiljaggavarapu@gmail.com>
2025-02-26 11:08:09 +0530

    20

--- constants/MenuImages.js
+++ constants/MenuImages.js
@@ -2,9 +2,7 @@ import potato from "@/assets/images/menu/potato.png"
 import tomato from "@/assets/images/menu/tomato.png"
 import biryani from "@/assets/images/menu/biryani.png"
 import chicken from "@/assets/images/menu/chicken.png"
-import frenchfry from "@/assets/images/menu/frenchfry.png"
 import palav from "@/assets/images/menu/palav.png"
-import pasta from "@/assets/images/menu/pasta.png"
 import soup from "@/assets/images/menu/soup.png"
 import fish from "@/assets/images/menu/fish.png"
 import paneer from "@/assets/images/menu/paneer.png"
@@ -14,9 +12,7 @@ export default [
     tomato,
     biryani,
     chicken,
-    frenchfry,
     palav,
-    pasta,
     soup,
     fish,
     paneer
