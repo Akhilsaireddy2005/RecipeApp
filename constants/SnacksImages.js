51a7c694fed1b139bcc45c4203c49ba60746919b
Akhilsaireddy2005 <akhiljaggavarapu@gmail.com>
2025-02-26 11:08:34 +0530

    22

---
+++ constants/SnacksImages.js
@@ -0,0 +1,11 @@
+import pizza from "@/assets/images/snacks/pizza.jpg"
+import burger from "@/assets/images/snacks/burger.jpg"
+import frenchfry from "@/assets/images/snacks/frenchfry.png"
+import pasta from "@/assets/images/snacks/pasta.png"
+
+export default [
+    pizza,
+    burger,
+    frenchfry,
+    pasta
+]
