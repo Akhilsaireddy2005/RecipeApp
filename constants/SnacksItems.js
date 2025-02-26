6a2ffb7ca37201e05771b7818699a9b7e8f2dbad
Akhilsaireddy2005 <akhiljaggavarapu@gmail.com>
2025-02-26 11:08:44 +0530

    23

---
+++ constants/SnacksItems.js
@@ -0,0 +1,22 @@
+export const Snacks_ITEMS = [
+    {
+        "id": 1,
+        "title": "Pizza",
+        "description":"Pizza is a delicious, versatile dish consisting of a baked dough base topped with sauce, cheese, and various toppings",
+    },
+    {
+        "id": 2,
+        "title": "burger",
+        "description": "A burger is a popular sandwich consisting of a cooked patty, usually beef, inside a sliced bun with various toppings and condiments.",
+    },
+    {
+        "id": 3,
+        "title": "French Fry",
+        "description": "French fries are crispy, golden-brown strips of potato, deep-fried to perfection and often served as a snack or side dish.",
+    },
+    {
+        "id": 4,
+        "title": "Pasta Recipe",
+        "description": "Pasta is a versatile Italian dish made from durum wheat, typically served with a variety of sauces like marinara, Alfredo, or pesto.",
+    }
+]
